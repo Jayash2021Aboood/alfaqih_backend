965fa28788c1788f55e3fbd829f2c0154c69f744
Aboood <AbdulmalekJayash@gmail.com>
2023-09-28 07:27:26 +0300

    Add SendWahtsappMessage Route for testing the code

--- routes/admin.php
+++ routes/admin.php
@@ -11,6 +11,8 @@ use Illuminate\Support\Facades\Route;
 Route::group([
   'prefix' => 'admin',
 ], function () {
+  Route::get('/sendMessage', [AuthenticationController::class, 'sendMessage']);
+
   Route::post('/auth/login', [AuthenticationController::class, 'login']);
 
   Route::post('/auth/login/verify', [AuthenticationController::class, 'verify']);
