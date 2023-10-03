9962562e8c3a9fd30a8fa70ba170cdb595749aca
Aboood <AbdulmalekJayash@gmail.com>
2023-10-03 19:45:43 +0300

    Create Parts tabel Migration

---
+++ database/migrations/2023_10_03_155443_create_parts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('parts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('price');
+            $table->string('img');
+            $table->longText('imgs')->nullable();
+            $table->longText('cars');
+            $table->longText('des');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('parts');
+    }
+};
